255d4b94db0a1888294e8b04f91488cb20e6e9be
Daniel Warna <danielwarna@Daniels-Mac.local>
2012-02-18 10:21:22 +0200

    Initial Commit

---
+++ FoodApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FoodApp
+//
+//  Created by Daniel Warna on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FoodApp/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  FoodApp
+//
+//  Created by Daniel Warna on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
